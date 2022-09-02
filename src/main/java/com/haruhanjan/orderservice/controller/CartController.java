32a198488513be48002c747ad73456c5cd8309ea
Hyeonjun Park <phjppo0918@gmail.com>
2022-09-02 15:06:41 +0900

    feat: cartController requsetMapping 추가

--- src/main/java/com/haruhanjan/orderservice/controller/CartController.java
+++ src/main/java/com/haruhanjan/orderservice/controller/CartController.java
@@ -2,10 +2,12 @@ package com.haruhanjan.orderservice.controller;
 
 import com.haruhanjan.orderservice.service.CartService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("cart")
 public class CartController {
     private final CartService cartService;
 }
