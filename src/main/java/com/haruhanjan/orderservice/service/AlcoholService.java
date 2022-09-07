3f4961928a2de87b356c64847b4134a8cfee0ec5
Hyeonjun Park <phjppo0918@gmail.com>
2022-09-07 21:53:20 +0900

    feat: alcohol service 설정

---
+++ src/main/java/com/haruhanjan/orderservice/service/AlcoholService.java
@@ -0,0 +1,33 @@
+package com.haruhanjan.orderservice.service;
+
+import com.haruhanjan.orderservice.dto.AlcoholResponse;
+import com.haruhanjan.orderservice.entity.Alcohol;
+import com.haruhanjan.orderservice.repository.AlcoholRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+import javax.persistence.EntityNotFoundException;
+
+@Service
+@RequiredArgsConstructor
+public class AlcoholService {
+
+    private final AlcoholRepository alcoholRepository;
+    InternalWebService internalWebService;
+
+    @KafkaListener(topics = "alcohol", groupId = "alcohol")
+    public void consume(String message) { //1
+
+        AlcoholResponse alcohol = internalWebService
+                .callApiGetAlcoholById(Long.parseLong(message));
+
+        alcoholRepository.save(alcohol.toEntity());
+    }
+
+    public Alcohol findById(Long id) {
+        return alcoholRepository.findById(id)
+                .orElseThrow(EntityNotFoundException::new);
+    }
+
+}
