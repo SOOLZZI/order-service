3f9dd5ee398363bb52cb30ba9754633143bdd4d7
Hyeonjun Park <phjppo0918@gmail.com>
2022-09-23 13:34:14 +0900

    fix: CartItemResponseDto 부분 Builder 추가

--- src/main/java/com/haruhanjan/orderservice/dto/CartItemResponseDto.java
+++ src/main/java/com/haruhanjan/orderservice/dto/CartItemResponseDto.java
@@ -9,11 +9,13 @@ import lombok.experimental.SuperBuilder;
 public class CartItemResponseDto extends ItemResponseDto {
     private Long cartId;
 
+    @Builder
     public CartItemResponseDto(Long alcoholId, Integer quantity, Long cartId) {
         super(alcoholId, quantity);
         this.cartId = cartId;
     }
 
+    @Builder
     public CartItemResponseDto(Long alcoholId, String alcoholName, Integer quantity, Integer price, Long cartId) {
         super(alcoholId, alcoholName, quantity, price);
         this.cartId = cartId;
